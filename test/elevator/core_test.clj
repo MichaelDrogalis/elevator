4469a870816ea568d896a6e7c2033344501c5de0
Michael Drogalis <mjd3089@rit.edu>
2013-05-20 11:17:22 -0400

    Cleaned up microtask tests.

--- test/elevator/core_test.clj
+++ test/elevator/core_test.clj
@@ -6,31 +6,30 @@
 
 (deftest downstream-outside-requests
   (are [floor]
-       (= :downstream (discretize?> floor :outside))
-       4 3 2 1))
+    (= :downstream (discretize?> floor :outside))
+    4 3 2 1))
 
 (deftest downstream-inside-requests
   (are [floor]
-       (= :downstream (discretize?> floor :inside))
-       4 3 2 1))
+    (= :downstream (discretize?> floor :inside))
+    4 3 2 1))
 
 (deftest upstream-outside-requests
   (are [floor]
-       (= :upstream (discretize?> floor :outside))
-       5 6 7))
+    (= :upstream (discretize?> floor :outside))
+    5 6 7))
 
 (deftest rejected-inside-requests
   (are [floor]
-       (= :rejected (discretize?> floor :inside))
-       5 6 7))
+    (= :rejected (discretize?> floor :inside))
+    5 6 7))
 
 (deftest discretizes-microtasks
-  (is (= [{:floor 4 :task :open-doors}]
-         (discretize {:floor 5 :direction :down} 4)))
-  (is (= [{:floor 4 :task :proceed} {:floor 3 :task :open-doors}]
-         (discretize {:floor 5 :direction :down} 3)))
-  (is (= [{:floor 4 :task :proceed}
-          {:floor 3 :task :proceed}
-          {:floor 2 :task :open-doors}]
-         (discretize {:floor 5 :direction :down} 2))))
+  (are [dst-floor tasks]
+    (= tasks (discretize {:floor 5 :direction :down} dst-floor))
+    4 [{:floor 4 :task :open-doors}]
+    3 [{:floor 4 :task :proceed} {:floor 3 :task :open-doors}]
+    2 [{:floor 4 :task :proceed}
+       {:floor 3 :task :proceed}
+       {:floor 2 :task :open-doors}]))
 
