1045c4a860e72263664ffe1687477313aabde051
Michael Drogalis <mjd3089@rit.edu>
2013-05-19 23:29:52 -0400

    Broke initial multimethod up into another.

--- src/elevator/core.clj
+++ src/elevator/core.clj
@@ -1,24 +1,29 @@
 (ns elevator.core)
 
+(defmulti downstream?
+  (fn [direction _ _] direction))
+
+(defmethod downstream? :up
+  [_ src-floor dst-floor]
+  (>= (- dst-floor src-floor) 0))
+
+(defmethod downstream? :down
+  [_ src-floor dst-floor]
+  (>= (- src-floor dst-floor) 0))
+
 (defmulti discretize?
   (fn [{:keys [floor direction]} dst-floor location]
-    (if (= :up direction)
-      [(>= (- dst-floor floor) 0) location]
-      [(>= (- floor dst-floor) 0) location])))
+    [(downstream? direction floor dst-floor) location]))
 
 (defmethod discretize? [true :outside]
-  [& _]
-  :downstream)
+  [& _] :downstream)
 
 (defmethod discretize? [false :outside]
-  [& _]
-  :upstream)
+  [& _] :upstream)
 
 (defmethod discretize? [true :inside]
-  [& _]
-  :downstream)
+  [& _] :downstream)
 
 (defmethod discretize? [false :inside]
-  [& _]
-  :rejected)
+  [& _] :rejected)
 
