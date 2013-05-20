657e7410b1946f9344187a0a165fe213539f2521
Michael Drogalis <mjd3089@rit.edu>
2013-05-19 23:15:07 -0400

    Basic start for discretization.

--- src/elevator/core.clj
+++ src/elevator/core.clj
@@ -1,6 +1,20 @@
 (ns elevator.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defmulti discretize?
+  (fn [{:keys [floor direction]} dst-floor location]
+    (if (= :up direction)
+      [(>= (- dst-floor floor) 0) location]
+      [(>= (- floor dst-floor) 0) location])))
+
+(defmethod discretize? [true :outside]
+  [& _]
+  :downstream)
+
+(defmethod discretize? [false :outside]
+  [& _]
+  :upstream)
+
+(defmethod discretize? [true :inside]
+  [& _]
+  :downstream)
+
--- test/elevator/core_test.clj
+++ test/elevator/core_test.clj
@@ -2,6 +2,25 @@
   (:require [clojure.test :refer :all]
             [elevator.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest downstream-outside-requests
+  (are [floor control]
+       (= :downstream (discretize? {:floor 4 :direction :down} floor control))
+       4 :outside
+       3 :outside
+       2 :outside
+       1 :outside))
+
+(deftest downstream-inside-requests
+  (are [floor control]
+       (= :downstream (discretize? {:floor 4 :direction :down} floor control))
+       3 :inside
+       2 :inside
+       1 :inside))
+
+(deftest upstream-outside-requests
+  (are [floor control]
+       (= :upstream (discretize? {:floor 4 :direction :down} floor control))
+       5 :outside
+       6 :outside
+       7 :outside))
+
